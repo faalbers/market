29395b0b3124530fe0cf7156bbc551bc7c5560f0
faalbers <chalbers@gmail.com>
2024-12-20 12:33:01 -0800

    quicken price export

---
+++ quickenCSVImport.py
@@ -0,0 +1,17 @@
+from market import Market, Quicken
+from pprint import pp
+from datetime import datetime
+import pandas as pd
+
+if __name__ == '__main__':
+    m = Market()
+    q = Quicken('database/2020.QIF')
+    symbols = q.get_securities()['symbol'].unique()
+    # print(symbols)
+    data = m.vault.get_data(['price'], symbols, update=True)['price']
+    dftn = pd.DataFrame(data).T['price'].dropna()
+    pp(dftn)
+    dftn.to_csv('Z:\\Quicken\\QuickenImport.csv', header=False, sep=',', encoding='utf-8')
+    del(m)
+
+
