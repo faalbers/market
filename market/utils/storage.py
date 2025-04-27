2de6f95519dfaef5322ea4d73e649df4b71ebf4d
faalbers <chalbers@gmail.com>
2025-04-27 09:50:30 -0700

    added backup

--- market/utils/storage.py
+++ market/utils/storage.py
@@ -1,4 +1,4 @@
-import pickle, os
+import pickle, os, glob, shutil
 
 def save(data, name):
     fileName = name+'.pickle'
@@ -14,3 +14,37 @@ def load(name):
         data = pickle.load(f)
     f.close()
     return data
+
+def backup(name):
+    basename = os.path.basename(name)
+    dirname = os.path.dirname(name)
+    filename ='%s.pickle' % name
+    backup_dir =  '%s/backup' % dirname
+    filename_backup ='%s/%s_01.pickle' % (backup_dir, basename)
+    filenames_backup_wildcard ='%s/%s_*.pickle' % (backup_dir, basename)
+
+    # create backup dir if it does not exist
+    if not os.path.exists(backup_dir): os.mkdir(backup_dir)
+    
+    backup_files = glob.glob(filenames_backup_wildcard)
+    backup_files = [os.path.normpath(filename).replace('\\', '/') for filename in backup_files]
+    backup_files.sort(reverse=True)
+    
+    # move files up a version
+    if filename_backup in backup_files:
+        # move files up
+        for filename_old in backup_files:
+            splits = filename_old.split(basename)
+            old_version = int(splits[1].strip('_').strip('.pickle'))
+            if old_version > 4:
+                os.remove(filename_old)
+                continue
+            new_version = old_version + 1
+            new_version = "{:02d}".format(new_version)
+            filename_new = '%s/%s_%s.pickle' % (backup_dir,basename,new_version)
+            shutil.move(filename_old, filename_new)
+
+    try:
+        shutil.copyfile(filename, filename_backup)
+    except FileNotFoundError:
+        pass
