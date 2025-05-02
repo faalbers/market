7a38e6725761e724602625aa7794b947e6d683bb
faalbers <chalbers@gmail.com>
2025-05-02 11:27:50 -0700

    added yfinance check

--- market/scrape/yahoof/yahoof.py
+++ market/scrape/yahoof/yahoof.py
@@ -11,7 +11,7 @@ class YahooF():
     def __init__(self):
         return
     
-    def multi_execs(self, exec_list):
+    def multi_execs(self, exec_list, yfinance_ok=False):
         count_done = 0
         failed = 0
         failed_total = 0
@@ -41,4 +41,20 @@ class YahooF():
                 self.logger.info('YahooF:  manually stopped multi_exec')
                 self.db.commit()
                 break
+            if (count_done % 100) == 0:
+                if yfinance_ok:
+                    if not self.yfinance_ok():
+                        break
+                    else:
+                        self.logger.info('YahooF:  yfinance still ok ...')
         self.logger.info('YahooF:  done: %s , failed: %s' % (count_done, failed_total))
+
+    def yfinance_ok(self):
+        ticker = yf.Ticker('AAPL')
+        try:
+            ticker.fast_info['lastPrice']
+        except Exception as e:
+            if str(e) == 'Too Many Requests. Rate limited. Try after a while.':
+                self.logger.info('YahooF:  Too Many Requests. Rate limited. Change VPN location')
+                return False
+        return True
