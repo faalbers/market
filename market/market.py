1a48168bb2d07528d0d43fdc7bb6addf14d3eaf2
faalbers <chalbers@gmail.com>
2025-05-02 11:32:10 -0700

    added bunch of stuff. Still work in progress

--- market/market.py
+++ market/market.py
@@ -7,11 +7,14 @@ from pprint import pp
 from .database import Database
 import pandas as pd
 from langchain_ollama import OllamaLLM
-from .utils import stop_text
+from .utils import stop_text, yfinancetest
 from .analysis import Analysis
 from .quicken import Quicken
 from .report import Report
 from .portfolio import Portfolio
+from datetime import datetime
+import calendar
+from dateutil.relativedelta import relativedelta
 
 class Market():
     @staticmethod 
@@ -67,6 +70,27 @@ class Market():
         self.log_queue.put_nowait(None)
         sys.excepthook = self.builtin_excepthook
         self.log_queue.close()
+    
+    def update(self):
+        self.vault.update(['update'])
+    
+    def update_weekly(self, tickers=None):
+        if not tickers or tickers.count == 0:
+            tickers = self.get_scraped_tickers()
+        # # catalog 'update'
+        # tickers.update(['update'])
+        # catalog 'fundamental'
+        equity_tickers = self.get_tickers(tickers.get_symbols(['EQUITY']))
+        equity_tickers.update(['fundamental'])
+
+    def update_nightly(self, tickers=None):
+        if not tickers or tickers.count == 0:
+            tickers = self.get_scraped_tickers()
+        tickers.update(['update'])
+    
+    def update_nightly_us_market(self):
+        tickers = self.get_us_market_tickers()
+        tickers.update(['update'])
 
     def get_tickers(self, symbols):
         return Tickers(symbols)
@@ -107,35 +131,52 @@ class Market():
         return Tickers(symbols_fixed)
 
     def get_scraped_tickers(self):
-        data_symbols = self.vault.get_data(['symbols_scraped'])['symbols_scraped']
-        symbols = set()
-        for scrape_name, scrape_symbols in data_symbols.items():
-            # for now only get yahoo_chart and yahoo_quote symbols
-            symbols.update(scrape_symbols.keys())
+        data_symbols = self.vault.get_data(['profile'])['profile']
+        symbols = sorted(data_symbols)
         return Tickers(symbols)
 
     def get_portfolio(self, update=False):
         return Portfolio(update)
 
-    def get_analysis(self, tickers=None, recache=False):
-        return Analysis(tickers, recache=recache)
+    def get_analysis(self, tickers):
+        return Analysis(tickers)
 
     def make_data_report(self):
         tickers = self.get_scraped_tickers()
         tickers.make_data_report()
 
+    def make_quicken_prices(self):
+        # quicken prices at end of month for import to quicken
+        if not yfinancetest(): return
+
+        # get last day of month string
+        now = datetime.now()
+        last_day = calendar.monthrange(now.year, now.month)[1]
+        if now.day == last_day:
+            date_string = now.strftime('%Y-%m-%d')
+        else:
+            last_month = now - relativedelta(months=1)
+            last_day = calendar.monthrange(last_month.year, last_month.month)[1]
+            last_month = datetime(year=last_month.year, month=last_month.month, day=last_day)
+            date_string = last_month.strftime('%Y-%m-%d')
+
+        quicken = self.get_quicken('database/2020.QIF')
+        symbols = quicken.get_securities()['symbol'].dropna().unique()
+        tickers = self.get_tickers(symbols)
+        chart = tickers.get_chart(end_date=date_string, update=True)
+        # chart = tickers.get_chart(end_date=date_string)
+        dftn = pd.Series()
+        for symbol, df in chart.items():
+            df_symbol = df.loc[:date_string]
+            if df_symbol.shape[0] == 0: continue
+            dftn[symbol] = df_symbol.iloc[-1]['Close']
+        dftn = dftn.dropna().round(2)
+        print(dftn)
+        dftn.to_csv('Z:\\Quicken\\QuickenImport.csv', header=False, sep=',', encoding='utf-8')
+
     # TODO these need to be checked
     def get_quicken(self, qif_file):
         return Quicken(qif_file)
-
-    def update_nightly(self, tickers=None):
-        if not tickers or tickers.count == 0:
-            tickers = self.get_scraped_tickers()
-        tickers.update(['update'])
-    
-    def update_nightly_us_market(self):
-        tickers = self.get_us_market_tickers()
-        tickers.update(['update'])
 
     def make_porfolios_report(self):
         quicken = self.get_quicken('database/2020.QIF')
