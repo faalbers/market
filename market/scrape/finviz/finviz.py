75665467a46c2f10f027e562ed849d291b65ae8f
faalbers <chalbers@gmail.com>
2025-06-03 08:39:48 -0700

    fix

--- market/scrape/finviz/finviz.py
+++ market/scrape/finviz/finviz.py
@@ -11,17 +11,17 @@ class Finviz():
 
     @sleep_and_retry
     @limits(calls=3, period=1)
-    def get_news(self, symbol):
+    def get_news_limited(self, symbol):
         try:
             ticker = finvizfinance(symbol)
             news = ticker.ticker_news()
         except Exception as e:
             # self.logger.error("Finviz:  ticker news error: %s: %s" % (symbol, e))
-            return pd.DataFrame()
-        return news
+            return (False, pd.DataFrame())
+        return (True, news)
     
     def request_news(self, symbol):
-        symbol_news = self.get_news(symbol)
+        symbol_news = self.get_news_limited(symbol)
         if len(symbol_news) > 0:
             self.pushAPIData(symbol, symbol_news)
 
