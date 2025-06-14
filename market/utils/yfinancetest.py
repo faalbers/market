ef1b37f0e844d4fd790fd6b2d43aa52fd75ebe78
faalbers <chalbers@gmail.com>
2025-06-14 15:53:26 -0700

    fix for ne yfinance version

--- market/utils/yfinancetest.py
+++ market/utils/yfinancetest.py
@@ -3,7 +3,8 @@ import yfinance as yf
 def yfinancetest():
     ticker = yf.Ticker('AAPL')
     try:
-        ticker.fast_info['lastPrice']
+        quoteType = ticker.fast_info['quoteType']
+        if len(quoteType) == 0: return False
     except Exception as e:
         if str(e) == 'Too Many Requests. Rate limited. Try after a while.':
             print('Too Many Requests. Rate limited. Change VPN location')
